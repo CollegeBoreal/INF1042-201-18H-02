f92f8fe6ea93b9530b5bbfc6420b9954e8b5d904
Kaouther1ab <31968993+Kaouther1ab@users.noreply.github.com>
2018-02-07 19:03:55 -0500

    Create Polymorphisme.scala

---
+++ 3.HOF/300108249/src/main/scala/Polymorphisme.scala
@@ -0,0 +1,13 @@
+object Polymorphisme {
+def findFirst(ss: Array[String], key: String): Int = {
+  def loop(n: Int): Int =
+    if (n >= ss.length) -1
+    else if (ss(n) == key) n
+    else loop(n + 1)
+
+  loop(0)
+}
+  def main(args: Array[String]): Unit = {
+    println("ID: " + findFirst(Array("Moi", "Toi", "Elle", "Lui"), "Elle"))
+  }
+}
