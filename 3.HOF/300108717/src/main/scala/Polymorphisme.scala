79cdb9d7f7b28adae70a79d8050c9c0265436ae0
Florynda <florechek@gmail.com>
2018-02-12 15:13:33 -0500

    Flore

--- 3.HOF/300108717/src/main/scala/Polymorphisme.scala
+++ 3.HOF/300108717/src/main/scala/Polymorphisme.scala
@@ -11,10 +11,11 @@ object Polymorphisme {
     loop(0)
   }
 
-  def findFirst(ss: Array[Int], key: Int) = {
+  def findFirst[A](as: Array[A], key: A) = {
+
     def loop(n: Int): Int =
-      if (n >= ss.length) -1
-      else if (ss(n) == key) n
+      if (n >= as.length) -1
+      else if (as(n) == key) n
       else loop(n + 1)
 
     loop(0)
@@ -22,7 +23,9 @@ object Polymorphisme {
 
 
     def main (args: Array[String]): Unit = {
-    println("ID: " + findFirst(Array("Moi", "Toi", "Elle", "Lui"), "Moi"))
-    println("ID: " + findFirst(Array(15, 5, 23, 2), 2))
+      println("ID: " + findFirst(Array("Moi", "Toi", "Elle", "Lui"), "Moi"))
+      println("ID: " + findFirst(Array(15, 5, 23, 2), 2))
+      println("ID: " + findFirst(Array(1.2, 3.4, .2, 1.0), 1.0))
   }
+
 }
