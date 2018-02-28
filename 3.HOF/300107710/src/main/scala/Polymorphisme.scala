38ae6b57958d3855ff0554a1564bb2171f02bff4
Bouchichi <bouchichi.mostafa@gmail.com>
2018-02-28 15:37:29 -0500

    Mostafa

--- 3.HOF/300107710/src/main/scala/Polymorphisme.scala
+++ 3.HOF/300107710/src/main/scala/Polymorphisme.scala
@@ -1,24 +1,59 @@
 object Polymorphisme {
 
-def findFirst[A](as: Array[A], key:A): Int = {
-  def loop(n: Int): Int =
-    if (n >= as.length) -1
-    else if (as(n) == key) n
-    else loop(n + 1)
+  def findFirst[A](as: Array[A], key: A): Int = {
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (as(n) == key) n
+      else loop(n + 1)
 
-  loop(0)
-}
-  // Remplacer (key:A) par (p:A => Boolean) page 25
-def lessThan = new Function2[Int, Int, Boolean] {
-  def apply(a: Int, b: Int) = a < b
+    loop(0)
   }
 
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    def go(n: Int): Boolean =
+      if (n >= as.length - 1) true
+      else if (!ordered(as(n), as(n + 1))) false
+      else go(n + 1)
+
+    go(0)
+  }
+
+  // Remplacer (key:A) par (p:A => Boolean) page 25
+  //def lessThan = new Function2[Int, Int, Boolean] {
+  //def apply(a: Int, b: Int) = a < b
+  //  }
+
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  def partial1[A, B, C](a: A, f: (A, B) => C): B => C = (b: B) => f(a, b)
+
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a, b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
 
   def main(args: Array[String]): Unit = {
-    println("ID: " + findFirst(Array("Moi", "Toi", "Elle", "Lui"), "Elle") )
-    println("ID: " + findFirst(Array(15, 5, 23, 2), 2))
-    println("ID: " + findFirst(Array(1.2, 3.4, 0.2, 1.0), 1.0))
-    //println("ID: " + findFirst(Array("Foo", "Bar", "Baz"), "Bar"))
-    println(lessThan(10, 20))
+
+    println("ID: " + findFirst(Array("Moi", "Toi", "Elle", "Lui"), (x: String) => x == "Elle") )
+    println("ID: " + findFirst(Array(15, 5, 23, 2),(x: Int) => x == 2))
+    println("ID: " + findFirst(Array(1.2, 3.4, 0.2, 1.0), (x: Double) => x == 1.0))
+    println("ID: " + findFirst(Array(3, 5, 2, 1), (x: Int) => x % 2 == 1.0))
+    println("ID: " + findFirst(Array(3, 5, 2, 1), (x: Int) => x % 2 == 0))
+
+    println("sorted:" + isSorted(Array(1, 2, 3), (x: Int, y: Int) => x <= y))
+    println("sorted:" + isSorted(Array(1, 2, 3), (x: Int, y: Int) => x <= y))
+    println("curry:" + curry((x: Int, y: Int) => x + y)(4)(2))
+    println("compose:" + compose((x: Int)=> x + 1, (y: Int) => y - 2)(2))
+
+    def f(x: Int, y: Int) = x + y
+    println("curry: " + curry((x: Int, y: Int) => x + y)(4)(4))
   }
+
 }
