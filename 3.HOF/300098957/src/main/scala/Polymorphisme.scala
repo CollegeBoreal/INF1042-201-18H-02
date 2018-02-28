fc62aa2c6a019900972d9a0db7c600e64bb7850f
setrar <setrar@valiha.com>
2018-02-28 15:28:57 -0500

    // Remplacer <key: A> par <p: A => Boolean> dans la (Listing 2.5 page 23)

--- 3.HOF/300098957/src/main/scala/Polymorphisme.scala
+++ 3.HOF/300098957/src/main/scala/Polymorphisme.scala
@@ -1,34 +1,48 @@
 object Polymorphisme {
 
   // page 23 Listing 2.3
-  /*
-  def findFirst(ss: Array[String], key: String): Int = {
-
-    def loop(n: Int): Int =
-      if (n >= ss.length) -1
-      else if (ss(n) == key) n
-      else loop(n + 1)
-
-    loop(0)
-  }
-  */
-
-  // Remplacer <key: A> par <p: A => Boolean> dans la (Listing 2.5 page 23)
   def findFirst[A](as: Array[A], p: A => Boolean): Int = {
 
     def loop(n: Int): Int =
-      if (n >= as.length)  -1
-      else if (as(n) == key) n
+      if (n >= as.length) -1
+      else if (p(as(n))) n
       else loop(n + 1)
 
     loop(0)
   }
 
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean =
+      if (n >= as.length - 1) true
+      else if (ordered(as(n), as(n + 1))) false
+      else go(n + 1)
 
-    def main(args: Array[String]): Unit = {
-    println("ID: " + findFirst(Array("Moi", "Toi", "Elle", "Lui"), "Elle"))
-    println("ID: " + findFirst(Array(15, 5, 23, 2 ), 2))
-    println("ID: " + findFirst(Array(1.2, 3.4, .2, 1.0 ), 1.0))
+    go(0)
+  }
+
+  def partial1[A,B,C](a: A, f: (A,B) => C): B => C = (b: B) => f(a,b)
+
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a,b)
+
+  def compose[A, B, C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
+
+  def main(args: Array[String]): Unit = {
+    println("ID: " + findFirst(Array("Moi", "Toi", "Elle", "Lui"), (x: String) => x == "Elle")) // Anonymous
+    println("ID: " + findFirst(Array(15, 5, 23, 2 ), (x: Int) => x == 2))
+    println("ID: " + findFirst(Array(1.2, 3.4, .2, 1.0 ), (x: Double) => x == 1.0))
+    println("ID: " + findFirst(Array(3, 5, 2, 1 ), (x: Int) => x % 2 == 0))
+    println("ID: " + findFirst(Array(-1, -2, 0, 4 ), (x: Int) => x > 0))
+
+    println("ID: " + isSorted(Array(1,2,3), (x: Int, y: Int) => x <= y)) // Test si Tri croissant
+    println("ID: " + isSorted(Array(1,2,3), (x: Int, y: Int) => x <= y)) // Test si Tri decroissant
+    println("ID: " + isSorted(Array(1,2,3), (x: Int, y: Int) => x <= y)) // Test si non trie
+
+    println("Curry: " + curry ((x: Int, y: Int) => x + y)(4)(4))
+
+    def f(x: Int, y: Int) = x + y
+    println("Compose: " + compose((x:Int) => x + 1, (y: Int) => y -2)(2))
+
   }
 
 }
