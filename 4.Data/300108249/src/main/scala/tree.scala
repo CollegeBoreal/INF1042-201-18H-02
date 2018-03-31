4f327b70bcde325af8d20c974e9bdb56c177e744
Kaouther1ab <31968993+Kaouther1ab@users.noreply.github.com>
2018-03-30 20:56:58 -0400

    Kaouther

--- 4.Data/300108249/src/main/scala/tree.scala
+++ 4.Data/300108249/src/main/scala/tree.scala
@@ -1,21 +1,27 @@
 sealed trait Tree[+A] {
+
   def size: Int = this match {
     case Leaf(_) => 1
     case Branch(l,r) => (l.size + r.size) + 1
   }
-
 }
-case class Leaf[A](value: A) extends Tree[A]
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
-object Tree{
-  //ex25
-  //ex29
 
+case class Leaf[A](value: A) extends Tree[A]
+
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+  // Exercice 3.25
+  // ...
+  // Exercice 3.29
   def main(args: Array[String]): Unit = {
 
-    assert(Branch(Leaf(2.4), Leaf(1.3)).size==3)
     assert(Leaf[Int](1).size==1)
-    assert(true)
-    println(Branch(Leaf(1.2), Leaf(2.3)).size)
+    assert(Branch[Double](Leaf[Double](2.4),Leaf[Double](1.3)).size==3)
+    assert(Branch[Char](
+      Branch(Leaf('S'),Leaf('E')),
+      Branch(
+        Branch(Leaf('R'),Leaf('G')),
+        Branch(Leaf('E'),Leaf('S')))).size==11)
   }
 }
