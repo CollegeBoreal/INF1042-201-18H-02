81ab2a5f36568a2d3e14d39c6bcf9dce7ef31363
kaouther1ab <kaouthar.fettah@yahoo.fr>
2018-03-30 20:44:56 -0400

    Kaouther

--- 4.Data/300108249/src/main/scala/tree.scala
+++ 4.Data/300108249/src/main/scala/tree.scala
@@ -3,19 +3,51 @@ sealed trait Tree[+A] {
     case Leaf(_) => 1
     case Branch(l,r) => (l.size + r.size) + 1
   }
-
 }
+
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
-object Tree{
-  //ex25
-  //ex29
 
+object Tree {
+  // Exercice 3.25
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1                              //on compte d'abord leaves
+    case Branch(l, r) => 1 + size(l) + size(r)      // puis les branch+leaves
+  }
+  // Exercice 3.26
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+  //Exercice 3.27
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+  //Exercice 3.28
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  // Exercice 3.29
   def main(args: Array[String]): Unit = {
-
-    assert(Branch(Leaf(2.4), Leaf(1.3)).size==3)
     assert(Leaf[Int](1).size==1)
-    assert(true)
-    println(Branch(Leaf(1.2), Leaf(2.3)).size)
+    assert(Branch[Double](Leaf[Double](2.4),Leaf[Double](1.3)).size==3)
+    assert(Branch[Char](
+      Branch(Leaf('S'),Leaf('E')),
+      Branch(
+        Branch(Leaf('R'),Leaf('G')),
+        Branch(Leaf('E'),Leaf('S')))).size==11)
+    //3.25
+
+    //3.26
+
+    //3.27
+
+    //3.28
+
+    //3.29
+
   }
 }
