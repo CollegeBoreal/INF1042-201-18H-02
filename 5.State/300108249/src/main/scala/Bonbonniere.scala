844c3541ef72b13c43dc21b4d55179d0f6abca24
Kaouther1ab <31968993+Kaouther1ab@users.noreply.github.com>
2018-04-02 15:59:25 -0400

    Kaouther

--- 5.State/300108249/src/main/scala/Bonbonniere.scala
+++ 5.State/300108249/src/main/scala/Bonbonniere.scala
@@ -2,11 +2,18 @@ sealed trait Input
 case object Coin extends Input
 case object Turn extends Input
 
-case class Machine (locked: Boolean, candies: Int, coins: Int)
+case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object Bonbonniere {
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int,Int)] = ???
+
+  // pattern matching for State
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int,Int)] = ??? // a besoin de sequence
+
   def main(args: Array[String]): Unit = {
-    assert(simulateMachine(List(Coin,Turn)).run(Machine(locked = true, 1, 0))==(1, 1))
+
+    assert(simulateMachine(List(Coin,Turn)).run(Machine(locked = true, 1, 0))==(1,1))
+
   }
+
 }
