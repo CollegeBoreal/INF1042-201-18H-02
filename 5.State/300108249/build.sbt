a723da203e11dac938004abde8d5f7498b79dfc3
kaouther1ab <kaouthar.fettah@yahoo.fr>
2018-04-02 14:50:47 -0400

    kaouther

---
+++ 5.State/300108249/build.sbt
@@ -0,0 +1,5 @@
+name := "300108249"
+
+version := "0.1"
+
+scalaVersion := "2.12.5"
---
+++ 5.State/300108249/src/main/scala/Bonbonniere.scala
@@ -0,0 +1,12 @@
+sealed trait Input
+case object Coin extends Input
+case object Turn extends Input
+
+case class Machine (locked: Boolean, candies: Int, coins: Int)
+
+object Bonbonniere {
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int,Int)] = ???
+  def main(args: Array[String]): Unit = {
+    assert(simulateMachine(List(Coin,Turn)).run(Machine(locked = true, 1, 0))==(1, 1))
+  }
+}
