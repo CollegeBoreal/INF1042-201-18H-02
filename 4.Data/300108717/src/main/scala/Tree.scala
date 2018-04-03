cc9650217aa928bb8d5d107053b141f64deebbb2
Florynda <florechek@gmail.com>
2018-04-03 12:31:30 -0400

    Flore Exercice 3.26 - 29

--- 4.Data/300108717/src/main/scala/Tree.scala
+++ 4.Data/300108717/src/main/scala/Tree.scala
@@ -10,19 +10,59 @@ case class Branch[A] (left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
-  // Exercice 3.25
-  // ...
-  // Exercice 3.29
 
-   def main(args: Array[String]): Unit= {
-     assert(Leaf[Int](1).size == 1)
-     assert(Branch(Leaf(2.4),Leaf(1.3)).size ==3)
-     assert(Branch[Char](Branch(Leaf('S'),Leaf('E')),
-       Branch
-       (Branch(Leaf('R'),Leaf('G')),
-         Branch(Leaf('E'),Leaf('S')))).size== 11)
-     //println(Branch(Leaf(2.4),Leaf(1.3)).size)
-   }
-     //assert(true)
+  // exercice 3.26
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(n) => n
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+
+  //Exercice 3.27
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => +(depth(l) max depth(r))
+  }
+
+  //Exercice 3.28
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(a) => Leaf(f(a))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  // Exercice 3.29
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+  def sizeViaFold[A](t: Tree[A]): Int =
+    fold(t)(a => 1)(1 + _ + _)
+
+  def main(args: Array[String]): Unit = {
+    assert(Leaf[Int](1).size == 1)
+    assert(Branch(Leaf(2.4), Leaf(1.3)).size == 3)
+    assert(Branch[Char](Branch(Leaf('S'), Leaf('E')),
+      Branch
+      (Branch(Leaf('R'), Leaf('G')),
+        Branch(Leaf('E'), Leaf('S')))).size == 11)
+
+
+    //3.26
+    assert(maximum(Leaf(5))==5)
+    assert(maximum(Branch(Leaf(2), Leaf(40)))== 40)
+    assert(maximum(Branch(Branch(Leaf(5),Leaf(8)),Leaf(3)))== 8)
+
+    //3.27
+    assert(depth(Leaf(2)) == 0)
+   // assert(depth(Branch(Leaf(2), Leaf(3)))== 1)
+
+    //3.28
+    assert(map(Branch(Leaf(50), Leaf(2)))(_ * 1.5) == Branch(Leaf(75.0), Leaf(3.0)))
+    assert(map(Leaf(4))(_ * 1.5) == Leaf(6.0))
+
+    //3.29
+    assert(sizeViaFold(Branch(Leaf(1), Leaf(2)))== 3)
+
+  }
 
 }
