a3ded0652414c5b7d2db40e388154e0fa8544b25
safaa <safaazaoui@gmail.com>
2018-04-04 15:02:57 -0400

    Merge branch 'master' of https://github.com/CollegeBoreal/INF1042-201-18H-02
    # Conflicts:
    #	4.Data/300108495/src/main/scala/tree.scala

--- 5.State/300108495/src/main/scala/Bonbonniere.scala
+++ 5.State/300108495/src/main/scala/Bonbonniere.scala
@@ -4,18 +4,20 @@ case object Coin extends Input
 case object Turn extends Input
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
+import State._
+
 object Bonbonniere{
-  def updaterule: Input => ( Machine => Machine) = ( i: Input) => (s: Machine) => (i, s) match {
+  def updateRule: Input => ( Machine => Machine) = ( i: Input) => (s: Machine) => (i, s) match {
     case (Turn, Machine(true, _, _)) => s
   }
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] =
-    for (
+    for {
       _ <- sequence(inputs.map(modify[Machine] _ compose updateRule))
       s <- get
-    ) yield (s.coins, s.candies)
+    } yield (s.coins, s.candies)
 
   def main(args: Array[String]): Unit = {
-    assert(simulateMachine(List(Coin, Turn)).run(Machine(locked = true, 1, 0))==(0,1))
+    assert(simulateMachine(List( Turn)).run(Machine(locked = true, 1, 0))._1==(0,1))
   }
 }
 
