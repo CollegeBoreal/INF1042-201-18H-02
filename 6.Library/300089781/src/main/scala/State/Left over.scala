96c37413e4b992f654c761934755280c484594c5
ameliedubois <fichou@hotmail.com>
2018-04-12 16:02:30 -0400

    Amelie

---
+++ 6.Library/300089781/src/main/scala/State/Left over.scala
@@ -0,0 +1,35 @@
+package state
+
+import scalaz.State
+import scalaz.Scalaz._
+
+case class LeftOver(size: Int)
+
+object LeftOver {
+  type Result[A] = State[LeftOver, A]
+
+  def getFromState(a: Int): Result[Int] = {
+
+    State[LeftOver, Int] {
+      case x => (LeftOver(x.size - a), a)
+    }
+  }
+  def addToState(a: Int): Result[Int] = {
+    State[LeftOver, Int] {
+      case x => (LeftOver(x.size + a), a)
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val res: Result[Int] =
+      for {
+      _<-addToState(20)
+      _<-getFromState(5)
+      _<-getFromState(5)
+      b<- getFromState(5)
+    } yield b
+
+    println(res(LeftOver(10)))
+    assert(res(LeftOver(10))==(LeftOver(15), 5))
+  }
+}
