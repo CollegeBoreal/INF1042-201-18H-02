0a84e7013c4eaa23ae079444b45b72555621f835
kaouther1ab <kaouthar.fettah@yahoo.fr>
2018-04-19 16:14:13 -0400

    kaouther

--- P.Projets/300108249/src/main/scala/state/TicTacToe.scala
+++ P.Projets/300108249/src/main/scala/state/TicTacToe.scala
@@ -80,5 +80,133 @@ trait State[S <: State[S]] {
   def playerTwoWin    : Boolean
   def isPlayerOneTurn : Boolean
   def generateStates  : Seq[S]
+}
+////tic tac toe
+class TicTacToe(playerOne: BestMoveFinder[TicTacToeState],
+                playerTwo: BestMoveFinder[TicTacToeState]) {
+  private var players = List(playerOne, playerTwo)
+  private var game: TicTacToeState =
+    new TicTacToeState(DIMENSION, DIMENSION)
+  def play = {
+    var moveNumber = 0
+    while(!game.isGameOver) {
+      println(s"Player ${moveNumber % 2 + 1} makes move:")
 
+      val player = players.head
+      game = player.move(game)
+      println(display(game))
+      players = players.tail :+ player
+
+      moveNumber += 1
+    }
+    if(game.playerOneWin) println("Player One win!")
+    else if(game.playerTwoWin) println("Player Two win!")
+    else println("Draw!")
+  }
+  def display(game: TicTacToeState) =
+
+    (1 to DIMENSION).map(row =>
+      (1 to DIMENSION).map(col => {
+        val p = Position(row, col)
+        if(game.playerOnePositions contains p) X_PLAYER
+        else if(game.playerTwoPositions contains p) O_PLAYER
+        else EMPTY_CELL
+      }).mkString).mkString("\n") + "\n"
+  final val DIMENSION  = 3
+  final val X_PLAYER   = "X"
+  final val O_PLAYER   = "O"
+  final val EMPTY_CELL = "."
+}
+///TicTacToeState
+case class Position(val row: Int, val col: Int)
+
+
+
+class TicTacToeState(val playerOnePositions : Set[Position],
+
+                     val playerTwoPositions : Set[Position],
+
+                     val availablePositions : Set[Position],
+
+                     val isPlayerOneTurn : Boolean,
+
+                     val winLength : Int) extends State[TicTacToeState] {
+
+
+
+  lazy val isGameOver  : Boolean =
+
+    availablePositions.isEmpty || playerOneWin || playerTwoWin
+
+
+
+  lazy val playerOneWin: Boolean = checkWin(playerOnePositions)
+
+
+
+  lazy val playerTwoWin: Boolean = checkWin(playerTwoPositions)
+
+
+
+  def generateStates: Seq[TicTacToeState] =
+
+    for(pos <- availablePositions.toSeq) yield makeMove(pos)
+
+
+
+  def makeMove(p: Position): TicTacToeState = {
+
+    assert(availablePositions.contains(p))
+
+    if(isPlayerOneTurn)
+
+      new TicTacToeState(
+
+        playerOnePositions + p,
+
+        playerTwoPositions,
+
+        availablePositions - p,
+
+        !isPlayerOneTurn,
+
+        winLength)
+
+    else new TicTacToeState(
+      playerOnePositions,
+      playerTwoPositions + p,
+      availablePositions - p,
+      !isPlayerOneTurn,
+      winLength)
+  def checkWin(positions: Set[Position]): Boolean =
+
+    directions.exists(winConditionsSatisified(_)(positions))
+
+  final val directions = List(leftDiagonal, rightDiagonal, row, column)
+
+  type StepOffsetGen = (Position, Int) => Position
+  def leftDiagonal : StepOffsetGen =
+
+    (pos, offset) => Position(pos.row + offset, pos.col + offset)
+  def rightDiagonal : StepOffsetGen =
+
+    (pos, offset) => Position(pos.row - offset, pos.col + offset)
+  def row : StepOffsetGen =
+
+    (pos, offset) => Position(pos.row + offset, pos.col)
+  private def column : StepOffsetGen =
+
+    (pos, offset) => Position(pos.row, pos.col + offset)
+  def winConditionsSatisified(step: StepOffsetGen)
+  (positions: Set[Position]): Boolean =
+  positions exists( position =>
+    (0 until winLength) forall( offset =>
+      positions contains step(position, offset)))
+
+  // Just additional constructor for convenience
+  def this(dimension: Int, winLength: Int) = this(
+    Set(), Set(), // positions of the players are empty initially
+    (for{row <- (1 to dimension) // initialize available positions
+         col <- (1 to dimension)} yield Position(row, col)).toSet,
+    true, winLength)
 }
