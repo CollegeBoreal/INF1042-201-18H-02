8affb1d29dd6810fc5fdd95956916119bb7f2cae
kaouther1ab <kaouthar.fettah@yahoo.fr>
2018-04-18 23:50:46 -0400

    kaouther

--- P.Projets/300108249/src/main/scala/state/MinMaxStrategyFinder.scala
+++ P.Projets/300108249/src/main/scala/state/MinMaxStrategyFinder.scala
@@ -4,32 +4,20 @@ import scalaz.State
 
 
 class MinMaxStrategyFinder[S <: State[S]] extends BestMoveFinder[S] {
-  // Here, and in other methods
-
-  // parameter s - is a current state of the game
 
   def move(s: S): S =
-
     if(s.isPlayerOneTurn) firstTurn(s).state
-
     else secondTurn(s).state
 
-
-
   def secondTurn(s: S): Outcome =
-
     bestMoveFinder(minimize, firstTurn, PLAYER_ONE_WIN, s)
 
-
-
   def firstTurn(s: S): Outcome =
-
     bestMoveFinder(maximize, secondTurn, PLAYER_ONE_LOOSE, s)
 
 
 
   def bestMoveFinder(strategy: Criteria, opponentMove: S => Outcome,
-
                      worstOutcome: Int, s: S): Outcome =
 
     if(s.isGameOver) Outcome(outcomeOfGame(s), s)
@@ -38,14 +26,15 @@ class MinMaxStrategyFinder[S <: State[S]] extends BestMoveFinder[S] {
         val outcome = opponentMove(state).cost
         if(strategy(outcome, acc.cost)) Outcome(outcome, state)
         else acc}}
+  
   def outcomeOfGame(s: S): Int =
     if (s.playerOneWin) PLAYER_ONE_WIN
     else if(s.playerTwoWin) PLAYER_ONE_LOOSE
     else DRAW
   case class Outcome(cost: Int, state: S)
-  val PLAYER_ONE_WIN   =  1 // the outcome, when first player wins
-  val PLAYER_ONE_LOOSE = -1 // the outcome, when second player wins
-  val DRAW             =  0 // the outcome, when nobody wins
+  val PLAYER_ONE_WIN   =  1
+  val PLAYER_ONE_LOOSE = -1
+  val DRAW             =  0
   type Criteria = (Int, Int) => Boolean
   def maximize: Criteria = (a, b) => a >= b
   def minimize: Criteria = (a, b) => a <= b
