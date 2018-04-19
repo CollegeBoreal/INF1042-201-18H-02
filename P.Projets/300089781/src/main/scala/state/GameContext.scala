58270c967e52f43431eab8625479987c68d23db0
ameliedubois <fichou@hotmail.com>
2018-04-19 18:35:10 -0400

    Amelie

--- P.Projets/300089781/src/main/scala/state/GameContext.scala
+++ P.Projets/300089781/src/main/scala/state/GameContext.scala
@@ -1,11 +1,13 @@
 package state
 
-trait GameContext extends Moves {
+import scalaz.State
 
-  def play(p1: Player, p2: Player): GameResult = p1.move == p2.move match {
-    case true => Tie
-    case _ if canBeat(p1.move, p2.move) => Win(p1)
-    case _ => Win(p2)
+sealed trait GameContext extends Moves {
+
+  def play(p1: Player, p2: Player): State[GameContext,GameResult] = p1.move == p2.move match {
+    case true => "Tie
+    case _ if canBeat(p1.move, p2.move) => "Win(p1)
+    case _ => "Win(p2)
   }
 
   def randomPlayer(name: String) = Player(name, randomMove)
@@ -14,20 +16,6 @@ trait GameContext extends Moves {
 trait RockPaperScissors extends GameContext {
   override protected val beats: Map[Move, List[Move]] =
     Map(Roche -> List(Ciseaux), Ciseaux -> List(Papier), Papier -> List(Roche))
-}
-
-trait RockPaperScissorsSpockLizard extends GameContext {
-
-  case object Spock extends Move
-
-  case object Lizard extends Move
-
-  override val moves = List(Roche, Papier, Ciseaux, Spock, Lizard)
-
-  override protected val beats: Map[Move, List[Move]] =
-    Map(Roche -> List(Ciseaux, Lizard), Ciseaux -> List(Papier, Lizard), Papier -> List(Roche, Spock),
-      Spock -> List(Ciseaux, Roche), Lizard -> List(Papier, Spock))
-
 }
 
 sealed trait GameResult
