eb901feb06da97020f0efcdfdc3ff030e1c7dfde
safaa <safaazaoui@gmail.com>
2018-04-23 19:27:45 -0400

    safaa

--- P.Projets/300108495/src/main/scala/state/StateExercice.scala
+++ P.Projets/300108495/src/main/scala/state/StateExercice.scala
@@ -2,6 +2,9 @@ package state
 import scalaz.State
 import scalaz.Scalaz._
 
+/*
+https://mega.nz/#!CUEQTYwI!YJ061tocIymyuLZqtBzDXSwcCnojr7DfLjIMpwEqjyA
+ */
 object StateExercise {
   trait Colour
   object Red extends Colour { override def toString = "Red"}
@@ -11,10 +14,10 @@ object StateExercise {
   type Bag = Map[Colour, Int]
   val empty: State[Bag, Bag] = state(Map[Colour, Int]())
 
-  def drawOne(soFar: Bag): State[Bag, Bag] = for(
+  def drawOne(soFar: Bag, seed: Int): State[Bag, Bag] = for(
     ball <- State[Bag, Colour]{s =>
       val (colours, counts) = s.toIndexedSeq.unzip
-      val ballIndex = (math.random * s.values.sum).toInt
+      val ballIndex = (seed * s.values.sum).toInt
       val selected = colours(
         counts.tail
           .view
@@ -29,17 +32,22 @@ object StateExercise {
     balls <- get[Bag];
     selection <- {
       assert(balls.values.sum >= n)   //Check the bag has enough left
-      (1 to n).foldLeft(empty)((accState, _) => accState.flatMap(drawOne))
+      (1 to n).foldLeft(empty)((accState, _) => accState.flatMap(x=>drawOne(x, seed=1)))
     }
   ) yield selection
 
-  val full: Bag = Map(Red -> 30, Green -> 20, Blue -> 10)
-  val (remaining, selected) = draw(10)(full)
 
   def main(args: Array[String]): Unit = {
-    assert(true)
+    val full: Bag = Map(Red -> 30, Green -> 20, Blue -> 10)
+    val (remaining, selected) = draw(10)(full)
     println(" Selected: "+selected)
     println("Remaining: "+remaining)
+    assert(true)
+    assert(draw(5)(full)==(Map(Red -> 30, Green -> 20, Blue -> 5),Map(Blue -> 5)))
+    assert(draw(30)(full)==(Map(Red -> 30, Green -> 0, Blue -> 0),Map(Blue -> 10, Green -> 20)))
+    assert(draw(60)(full)==(Map(Red -> 0, Green -> 0, Blue -> 0),Map(Red -> 30,Blue -> 10, Green -> 20)))
+
+
 
   }
 }
