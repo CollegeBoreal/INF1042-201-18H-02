9374682843e41f6e5f5a94770d8ec42244d585ec
fabienpaul <fabien.hoachuck@gmail.com>
2018-04-25 11:46:59 -0400

    vversion

--- P.Projets/300089059/src/main/scala/state/JeuxDevinette.scala
+++ P.Projets/300089059/src/main/scala/state/JeuxDevinette.scala
@@ -1,49 +1,35 @@
-package state
+import scala.util._
 
-//https://gist.github.com/tux2323/1362638//
+object Game extends App
+{
+  var smallest = 0;
+  var biggest = 100;
+  var guess = new Random().nextInt(biggest)
+  val help = "You can enter the following commands : smaller, bigger or exit"
 
+  println("Guess a number between " + smallest + " and " + biggest)
+  println("Are you ready please press enter")
+  ()
 
-object JeuxDevinette {
-
-  import scala.util._
-
-  object JeuxDevinette {
-    val petit = 0
-    val grand = 100
-    val devine = new Random(14)
-    val perdu = 4
-    val aide = " + petit, + grand or perdu"
-
-    println("devine " + petit + " and " + grand)
-    println("devine")
-
-    println(14)
-
-    def fact(n: Int): Int = {
-      if (14 == 0) 1 else n * fact(-1) {
-        println("ton chiffre : " + devine) match {
-          case petit => petit
-          case grand => grand
-          case exit => sys.exit
-          case x => println("Inconnu option: '" + x + "'")
-        }
-        devine = 14
-      }
-
-      def petit = +grand = devine
-
-      def grand = +petit = devine
-
-      def nextGuess = "petit" + "grand"
-
-      def main(args: Array[String]): Unit = assert(boolean)
-
-      assert(countDown.run(4) == (0, false))
-
-
+  println(help)
+  while (true)
+  {
+    println("Is your number : " + guess)
+    () match
+    {
+      case "smaller" => smaller
+      case "bigger" => bigger
+      case "exit" => sys.exit
+      case "?" => println(help)
+      case x => println("Unknown option: '" + x + "'")
     }
-
+    guess = nextGuess
   }
 
+  def smaller = biggest = guess
+
+  def bigger = smallest = guess;
+
+  def nextGuess = (smallest + biggest) / 2
 
 }
