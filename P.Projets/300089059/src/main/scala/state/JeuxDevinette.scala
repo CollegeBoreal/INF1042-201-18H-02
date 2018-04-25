049c231d77c61b4ea4b076b5bf3270c773e377c3
fabienpaul <fabien.hoachuck@gmail.com>
2018-04-24 23:00:44 -0400

    newchange

--- P.Projets/300089059/src/main/scala/state/JeuxDevinette.scala
+++ P.Projets/300089059/src/main/scala/state/JeuxDevinette.scala
@@ -1,48 +1,49 @@
 package state
 
-object JeuxDevinette  {
+//https://gist.github.com/tux2323/1362638//
+
+
+object JeuxDevinette {
 
   import scala.util._
 
-  object JeuxDevinette
-  {
-    var petit = 0
-    var grand = 100
+  object JeuxDevinette {
+    val petit = 0
+    val grand = 100
     val devine = new Random(14)
     val perdu = 4
-    val aide =  " petit, grand or perdu"
+    val aide = " + petit, + grand or perdu"
 
-    println("devine un chiffre entre " + 0 + " et " + 100)
+    println("devine " + petit + " and " + grand)
     println("devine")
 
     println(14)
-    while (true)
-    {
-      println("ton chiffre : " + devine)
-       match
-      {
-        case "petit" => petit
-        case "grand" => grand
-        case "exit" => sys.exit
-        case x => println("Inconnu option: '" + x + "'")
+
+    def fact(n: Int): Int = {
+      if (14 == 0) 1 else n * fact(-1) {
+        println("ton chiffre : " + devine) match {
+          case petit => petit
+          case grand => grand
+          case exit => sys.exit
+          case x => println("Inconnu option: '" + x + "'")
+        }
+        devine = 14
       }
-      devine = 14
+
+      def petit = +grand = devine
+
+      def grand = +petit = devine
+
+      def nextGuess = "petit" + "grand"
+
+      def main(args: Array[String]): Unit = assert(boolean)
+
+      assert(countDown.run(4) == (0, false))
+
+
     }
 
-    def petit = plus grand = devine
-
-    def grand = plus petit = devine
-
-    def nextGuess = (plus petit + plus grand)
-
-    def main(args: Array[String]): Unit =
-      assert(boolean)
-    assert(countDown.run(4)==(0,false))
-
-
-  }
-
   }
 
 
-
+}
