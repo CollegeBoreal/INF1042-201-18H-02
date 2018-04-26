0a4c9b376c0974ad20c5c742b117ae676b1522a6
fabienpaul <fabien.hoachuck@gmail.com>
2018-04-25 21:08:58 -0400

    dev

--- P.Projets/300089059/src/main/scala/state/JeuxDevinette.scala
+++ P.Projets/300089059/src/main/scala/state/JeuxDevinette.scala
@@ -1,37 +1,58 @@
 package state
-/* */
+/*         https://gist.github.com/tux2323/1362638             */
+
 import scala.util._
 
-object Game extends App
-{
-  val smallest = 0;
-  val biggest = 100;
+
+
+object Game {
+  var smallest = 0
+  var biggest = 100
   val guess = new Random().nextInt(biggest)
   val help = "You can enter the following commands : smaller, bigger or exit"
 
   println("Guess a number between " + smallest + " and " + biggest)
-  println("Are you ready please press enter")
+  println("Are you ready  press enter")
   readLine()
 
   println(help)
-  while (true)
-  {
-    println("Is your number : " + guess)
-    readLine() match
-    {
-      case "smaller" => smaller
-      case "bigger" => bigger
-      case "exit" => sys.exit
-      case "?" => println(help)
-      case x => println("Unknown option: '" + x + "'")
+
+
+  def main(args: Array[String]): Unit = {
+    val count = 0
+    val help = "You can enter the following commands : smaller, bigger or exit"
+
+  }
+  def count(number: Int): Int = {
+    def go(guess: Int, acc: Int): Int = {
+
+      if (acc == 0) {
+        println("Is your number:" + help)
+        0
+      } else {
+        println("Is your number : " + guess)
+        readLine() match {
+          case "smaller" => smaller
+          case "bigger" => bigger
+          case "exit" => sys.exit
+          case "?" => println(help)
+          case x => println("Unknown option: '" + x + "'")
+        }
+
+        def smaller = biggest = guess
+
+        def bigger = smallest = guess
+
+        def nextGuess = (smallest + biggest) / 2
+
+        go(nextGuess, acc - 1)
+      }
+
     }
-    guess = nextGuess
+
+    go(0, number)
   }
 
-  def smaller = biggest = guess
-
-  def bigger = smallest = guess;
-
-  def nextGuess = (smallest + biggest) / 2
 
 }
+
