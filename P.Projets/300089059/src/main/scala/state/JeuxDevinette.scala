bd611554a1a060ac48d03d3d8dddecce4d179c44
fabienpaul <fabien.hoachuck@gmail.com>
2018-04-26 10:33:41 -0400

    devinenew

--- P.Projets/300089059/src/main/scala/state/JeuxDevinette.scala
+++ P.Projets/300089059/src/main/scala/state/JeuxDevinette.scala
@@ -1,5 +1,10 @@
 package state
-/*         https://gist.github.com/tux2323/1362638             */
+
+/*
+Exemple d un jeux sur scala avec State monad.
+
+  https://gist.github.com/tux2323/1362638
+         */
 
 import scala.util._
 
@@ -8,51 +13,47 @@ import scala.util._
 object Game {
   var smallest = 0
   var biggest = 100
+  val number= 25
   val guess = new Random().nextInt(biggest)
   val help = "You can enter the following commands : smaller, bigger or exit"
 
   println("Guess a number between " + smallest + " and " + biggest)
-  println("Are you ready  press enter")
-  readLine()
-
-  println(help)
-
+  println("Is your number:" + guess)
+  println("You can enter the following commands : smaller, bigger or exit: ")
 
   def main(args: Array[String]): Unit = {
-    val count = 0
     val help = "You can enter the following commands : smaller, bigger or exit"
 
   }
   def count(number: Int): Int = {
-    def go(guess: Int, acc: Int): Int = {
+    def go(guess: Int): Int = {
 
-      if (acc == 0) {
-        println("Is your number:" + help)
-        0
+      if (guess = number) {
+
+        println("Felicitation")
+
+        )
+        100
+      if (guess => number)
+
       } else {
-        println("Is your number : " + guess)
-        readLine() match {
-          case "smaller" => smaller
-          case "bigger" => bigger
-          case "exit" => sys.exit
-          case "?" => println(help)
-          case x => println("Unknown option: '" + x + "'")
+        println("Is your number:" + guess)
+
         }
 
-        def smaller = biggest = guess
+        def smaller = guess
 
-        def bigger = smallest = guess
+        def bigger = guess
 
         def nextGuess = (smallest + biggest) / 2
 
-        go(nextGuess, acc - 1)
+        go(nextGuess)
       }
 
     }
 
-    go(0, number)
+    go(number)
   }
 
 
 }
-
